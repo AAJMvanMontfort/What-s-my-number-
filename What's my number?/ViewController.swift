b9bc0c7d9d19158c08b2332d0b51e28358f100f8
AAJMvanMontfort <lout@me.com>
2019-05-12 09:34:54 +0200

    Initial Commit

---
+++ What's my number?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What's my number?
+//
+//  Created by AAJM van Montfort on 12/05/2019.
+//  Copyright © 2019 AAJM van Montfort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ What's my number?Tests/What_s_my_number_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  What_s_my_number_Tests.swift
+//  What's my number?Tests
+//
+//  Created by AAJM van Montfort on 12/05/2019.
+//  Copyright © 2019 AAJM van Montfort. All rights reserved.
+//
+
+import XCTest
+@testable import What_s_my_number_
+
+class What_s_my_number_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
